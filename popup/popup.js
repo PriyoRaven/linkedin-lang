438871c8a8ade1b5934cc49b59da9ea6b8b42553
Raven <priyadarsi02@gmail.com>
2024-11-09 19:35:02 +0530

    Redeclare tailwind into the project.

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,9 @@
+// popup/popup.js
 document.addEventListener("DOMContentLoaded", async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   if (tab.url.includes("linkedin.com")) {
     document.getElementById("first-popup").classList.add("hidden");
-    document.querySelector("#translatorOptions").classList.remove("hidden");
+    document.getElementById("inside-LinkedIn").classList.remove("hidden");
   }
 });
