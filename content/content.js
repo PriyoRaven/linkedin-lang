892fa068ccd8e35bf58d42ae79af2040b644bfee
Raven <priyadarsi02@gmail.com>
2024-11-12 22:10:13 +0530

    Change the text translate into icon for better reference

--- content/content.js
+++ content/content.js
@@ -6,9 +6,17 @@ function addTranslateToChats() {
   chatMessages.forEach((message) => {
     if (!message.querySelector(".translate-text")) {
       const translateText = document.createElement("div");
-      translateText.textContent = "Translate";
       translateText.classList.add("translate-text");
 
+      // Image element is here
+      const translateImage = document.createElement("img");
+      translateImage.src = chrome.runtime.getURL("images/testimg.png");
+      translateImage.alt = "Translate";
+      translateImage.classList.add("translate-image");
+      // translateImage.style.width = "20px";
+      // translateImage.style.height = "20px";
+
+      translateText.appendChild(translateImage);
       message.appendChild(translateText);
     }
   });
